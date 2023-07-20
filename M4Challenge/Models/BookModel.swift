1a482774a3bb328f15eae3b881611aa4fdaa7906
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-20 19:57:14 -0400

    initial commit, added json file and created initial model for book data

---
+++ M4Challenge/Models/BookModel.swift
@@ -0,0 +1,19 @@
+//
+//  BookModel.swift
+//  M4Challenge
+//
+//  Created by Chuck Belcher on 7/20/23.
+//
+
+import Foundation
+
+
+class BookModel: Identifiable, Decodable {
+    var id: Int
+    var title: String
+    var author: String
+    var isFavourite: Bool
+    var currentPage: Int
+    var rating: Int
+    var content: [String]
+}
---
+++ M4Challenge/Views/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M4Challenge
+//
+//  Created by Chuck Belcher on 7/20/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}

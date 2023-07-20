bf0597ce32c8279338dff52e11f49377fc644152
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-20 19:38:34 -0400

    Initial Commit

---
+++ M4Challenge/M4ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4ChallengeApp.swift
+//  M4Challenge
+//
+//  Created by Chuck Belcher on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct M4ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
